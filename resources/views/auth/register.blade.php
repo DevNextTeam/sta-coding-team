4d258375caf8acb63dc2d918556ee8c3d61d0c04
DevNext Team <devnextteam@gmail.com>
2026-09-16 15:08:39 +0800

    Password Minimum Length

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -80,6 +80,7 @@
                     type="password"
                     name="password"
                     required
+                    minlength="12"
                     autocomplete="new-password"
                     placeholder="Create a password"
                     class="w-full px-4 py-3 rounded-xl border border-[#D9D3C7]
